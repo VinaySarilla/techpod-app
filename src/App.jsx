a285c03419764ccbc0238e103cfdbd260e17974b
VinaySarilla <vinayknown@gmail.com>
2022-05-06 21:50:26 +0530

    set app height

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./index.css";
 import AboutCard from "./components/AboutCard";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("resize", appHeight);
+    appHeight();
+  }, []);
+
+  const appHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  };
 
   return (
     <div className="font-inter">
